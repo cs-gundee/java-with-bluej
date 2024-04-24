aa7ad046b64a5a9cc6a115583972172b3ba0e550
cs-gundee <66056983+cs-gundee@users.noreply.github.com>
2024-04-24 22:25:08 +0800

    Update Pet.java

--- polymorphism-example2/Pet.java
+++ polymorphism-example2/Pet.java
@@ -8,7 +8,4 @@ public class Pet
     
     public String getUilderName() { return this.uilderName; }
     public void setUilderName(String uname) { this.uilderName = uname; }
-    
-    public String speak() { return "Тэжээвэр амьтан"; }
-    public int getLegCount() { return -1; }
 }
